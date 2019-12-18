badd8bf19194afc542e2853ed0475c3bcdb89729
GitProger <51924999+GitProger@users.noreply.github.com>
2019-12-18 13:25:16 +0300

    Create curlbl.pl

---
+++ curlbl.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+my $__USAGE__ = "Usage: build [filelist] [target binary]\n";
+
+sub build_with_curl {
+    my ($fname, $objname) = @_;    
+    my $cmd = "gcc -s -O2 " . $fname . " -lcurl -o " . $objname;
+    print "Running: $cmd\n";
+    print "-" x (9 + length($cmd)), "\n";
+    print `$cmd`;
+}
+
+if ($#ARGV > 1) {
+    my $flist = "";
+    my $target = pop(@ARGV);
+    for (my $i = 0; $i < scalar(@ARGV); $i++) {
+        $flist = $flist . " " . $ARGV[$i];
+	} 
+    build_with_curl($ARGV[0], $target);
+    exit 0;
+} else {
+    print __USAGE__;
+    exit -1;
+}
