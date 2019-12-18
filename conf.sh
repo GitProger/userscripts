df20622fbc234f259d00793de6452923b09ffc19
GitProger <51924999+GitProger@users.noreply.github.com>
2019-12-18 13:14:19 +0300

    Create conf.sh

---
+++ conf.sh
@@ -0,0 +1,9 @@
+alias f9="xset -display :0.0 dpms force off"
+alias sysmon="gnome-system-monitor"
+alias md=mkdir
+alias cls=clear
+alias py=python3
+
+cpp_ol_comp() {
+    g++ -Wall -std=c++11 -O2 main.cpp -o o
+}
