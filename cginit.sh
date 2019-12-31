300295a0070f75bdd19b82076c300c268d6c3a9e
GitProger <51924999+GitProger@users.noreply.github.com>
2019-12-31 15:12:54 +0300

    Update cginit.sh

--- cginit.sh
+++ cginit.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+#####################################
+#  Script initializing for Apache2  #
+#####################################
+
 sudo a2enmod cgid
 sudo systemctl restart apache2
 sudo cp $1 /usr/lib/cgi-bin/$1
-sudo chmod 705 /usr/lib/cgi-bin/$1
+#sudo chmod 705 /usr/lib/cgi-bin/$1
+sudo chmod 774 /usr/lib/cgi-bin/$1
