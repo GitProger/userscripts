5e7f6db1f426f2e17a4459a9ee2f8815e351b4e8
GitProger <51924999+GitProger@users.noreply.github.com>
2020-02-16 23:29:00 +0300

    Create coder.pl

---
+++ coder.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+sub getfile {
+    my $filename = $_[0];
+    my $file;
+    my $res = "";
+    if (!open($file, "<:encoding(UTF-8)", $filename)) {
+        print "Could not open file '$filename' $!";
+        return "";
+    }
+    while (my $s = <$file>) {
+        $res .= $s;
+    }
+    close($file);
+    return $res;
+}
+
+sub main {
+    my $n = scalar(@_);
+    if ($n != 1 && $n != 2) {
+        print "Usage:\tcoder [file] [-e string]\n";
+        print "\t'eval pack('H*', \$code);' in your programs\n\n";
+        return -1;
+    };
+    my $code = "";
+    if ($n == 1) {
+        $code = getfile(shift);
+    } elsif (shift eq "-e") {
+        $code = shift;
+    } else {
+        print "Invalid key.\n";
+    }
+
+    my $obf = unpack("H*", $code);
+    # eval pack('H*', $var);
+    print $obf;
+}
+
+exit(main(@ARGV));
