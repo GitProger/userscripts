72b6e6522e71a5508b4ca2d40d7b81a68444beb6
GitProger <51924999+GitProger@users.noreply.github.com>
2020-02-18 20:39:11 +0300

    Update cgiprint.py

--- cgiprint.py
+++ cgiprint.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 '''
     Use '<meta charset="utf-8">' or 
     'Content-type: text/html; charset=utf-8' instead of 'Content-type: text/html'
 '''
 
 import sys
+__standart_print = print
+
 def cgiprint(*argv, sep=" ", end="\n", encoding="utf-8", file=sys.stdout, flush=True):
     '''
-        Warning: Only python3
-        This is 'print' function for cgi output, works like standart python3's 'print'
+        This is 'print' function for cgi Unicode output, 
+        works like standart python3's 'print'
     '''
-    __local_write = lambda obj: file.buffer.write(
+    local_write = lambda obj: file.buffer.write(
         str(obj).encode(encoding)
     )
     for i in range(len(argv)):
-        __local_write(argv[i])
+        local_write(argv[i])
         if i != len(argv) - 1:
-            __local_write(sep)
-    __local_write(end)
+            local_write(sep)
+    local_write(end)
     if flush:
         file.buffer.flush()
 
@@ -28,3 +29,10 @@ def assign():
         Assign standart 'print' to 'cgiprint.cgiprint'
     '''
     print = cgiprint
+
+def cancel():
+    '''
+        Cancels 'cgiprint.assign'
+    '''
+    global __standart_print
+    print = __standart_print
