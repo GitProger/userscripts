71bddcf10f4370499fa3420b0974582e33d10027
GitProger <51924999+GitProger@users.noreply.github.com>
2020-03-06 20:14:33 +0300

    Create net-scan.pl

---
+++ net-scan.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+my $PACK_COUNT = 2;
+
+sub usage {
+    print "Usage: net-scan [beginAddr] [endAddr]\n\tScan all addresses in [beginAddr..endAddr]\n";
+}
+
+sub ip_to_int {
+    my ($i0, $i1, $i2, $i3) = (shift =~ m/(\d+)\.(\d+)\.(\d+)\.(\d+)/);
+    return ($i0 << 24) + ($i1 << 16) + ($i2 << 8) + $i3;
+}
+
+sub int_to_ip {
+    our $ip = shift;
+    sub sep {
+        my $res = $ip % 0x100;
+        $ip = int($ip / 0x100);
+        return $res;
+    }
+    my $i3 = sep(); my $i2 = sep(); my $i1 = sep(); my $i0 = sep();
+    return "$i0.$i1.$i2.$i3";
+}
+
+sub check {
+    my $host = shift;
+    `ping -c $PACK_COUNT $host`;
+    return $?;
+}
+
+sub scan {
+    my ($beg, $end) = (ip_to_int(shift), ip_to_int(shift));
+    for (my $addr = $beg; $addr <= $end; $addr++) {
+        my $host = int_to_ip($addr);
+        if (check($host) == 0) {
+            print "Ok: $host\n";
+        }
+    }
+}
+
+sub main {
+    if ($#_ < 0) {
+        usage();
+        return -1;
+    }
+    my $beg = shift;
+    my $end = "";
+    if ($#_ == -1) {
+        $end = $beg;
+    } else {
+        $end = shift;
+    }
+    scan($beg, $end);
+    return 0;
+}
+
+exit(main(@ARGV));
