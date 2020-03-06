b1637bcee2725d0db39f7952fcb96cd34c9a1aa2
GitProger <51924999+GitProger@users.noreply.github.com>
2020-03-06 13:38:16 +0300

    Update build.pl

--- build.pl
+++ build.pl
@@ -2,21 +2,13 @@
 
 use strict;
 use warnings;
-
-my $__USAGE__ = "Usage: build main-file final-object [libs]\n";
-my $__FLAGS__ = "-s -O2";
-my $__COMPILER__ = "gcc";
-my %__LIBMAP__ = (
-    "curses"  =>  "-lcurses",
-    "curl"    =>  "-lcurl",
-    "gtk"     =>  "`pkg-config --cflags --libs gtk+-3.0`",
-);
+use config;
 
 sub build {
     my ($fname, $objname) = (shift, shift);    
-    my $cmd = "$__COMPILER__ $fname $__FLAGS__ -o $objname ";
+    my $cmd = "${config::COMPILER} $fname ${config::FLAGS} -o $objname ";
     foreach my $curlib (@_) {
-        $cmd .= $__LIBMAP__{$curlib} . " "
+        $cmd .= $config::LIBS{$curlib} . " "
     }
     print "Running: $cmd\n";
     print "-" x (9 + length($cmd)), "\n";
@@ -28,9 +20,10 @@ sub main {
         build(@_);
         return 0;
     } else {
+        my $__USAGE__ = "Usage: build [main file] [final object] [libs]\n";
         print $__USAGE__;
         return -1;
     }
 }
 
-exit(main(@ARGV))
+exit(main(@ARGV));
