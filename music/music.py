4c035a754287c5185d7577d50113c28f85836d67
GitProger <51924999+GitProger@users.noreply.github.com>
2020-04-10 21:46:54 +0300

    Update music.py

--- music/music.py
+++ music/music.py
@@ -1,28 +1,30 @@
 #!/usr/bin/env python3
+#
+# Use this file to comfortable music download with 'MediaSave'
+#
 import sys, os, re;
 from getpass import getuser
+from mutagen.easyid3 import EasyID3
 
 def main(argv: list):
     __music__ = "/home/" + getuser() + "/Музыка/"
     __downloads__ = "/home/" + getuser() + "/Загрузки/"
     for f in os.listdir(__downloads__):
-        if re.search("\.mp3$", f):
+        if re.search(".mp3$", f):
             old = __downloads__ + f
-            if len(argv) < 2:
-                new = __music__ + f
-            elif "-n" in argv:
-                print(f[:-len(".mp3")], "to:", end=" ")
-                new = __music__ + input() + ".mp3"
-            else:
-                print("Invalid argument:", argv[1])
-                return -1;
+            if "-i" in argv: # ignore
+                continue
+
+            new = __music__ + input(f[:-len(".mp3")] + " to: ") + ".mp3"
             os.renames(old, new)
-            if "-w" in argv:
-                song = input("Song name: ")
-                auth = input("Author: ")
-                albm = input("Album: ")
-                year = input("Year: ")
-                os.system("./mp3ed -n " + song + " -a " + auth + " -al " + albm + " -y " + year)
+
+            audio = EasyID3("example.mp3")
+            audio['title'] = input("Title: ")
+            audio['artist'] = input("Artist: ")
+            audio['album'] = input("Album: ")
+            audio['composer'] = input("Composer: ")
+            audio.save()
+
     return 0
 
 if __name__ == "__main__":
