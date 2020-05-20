ffda6452144e36a7fa2f9e68a510ebc46b1a4a82
GitProger <51924999+GitProger@users.noreply.github.com>
2020-05-20 15:23:31 +0300

    Create no-covid-19.js

---
+++ js/no-covid-19.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         No COVID-19
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Remove annoying "#stayhome" elements
+// @author       You
+// @match        https://vk.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var stayhome_hashtag = document.getElementsByClassName("CovidLogo__hashtag")[0];
+    stayhome_hashtag.remove();
+    var stayhome_info = document.getElementById("l_covid19");
+    stayhome_info.remove();
+    var stayhome_bot = document.getElementById("ui_rmenu_peer_-22822305");
+    stayhome_bot.remove();
+    var stayhome_tooltip = document.getElementsByClassName("CovidTooltip")[0];
+    stayhome_tooltip.remove();
+
+    document.getElementsByClassName("ui_rmenu_sep").forEach(function (cur, i, arr) {
+        cur.remove(); // remove "#stayhome bot"`s separators
+    });
+
+    var stayhome_popup = document.getElementsByClassName("CovidLogo")[0];
+    stayhome_popup.removeAttribute("onmouseover"); // remove pop-up #stayhome menu
+    stayhome_popup.className = "top_home_link fl_l"; // set default logo
+})();
