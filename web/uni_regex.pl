5e2c822454ca1227f674ec06019e0e01188189fd
GitProger <51924999+GitProger@users.noreply.github.com>
2020-06-20 20:29:40 +0300

    Update uni_regex.pl

--- web/uni_regex.pl
+++ web/uni_regex.pl
@@ -2,18 +2,15 @@
 use strict;
 use warnings;
 use CGI;
-print "Content-type: text/plain; charset=UTF8\n\n";
+print "Content-type: text/plain\n\n";
 
 my $cgi = CGI->new();
 my $t = $cgi->param("type") || "";
 $_ = $cgi->param("value") || "";
 
-sub get {
-    return 
-        $t eq "email" ? /(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)/i :
-        $t eq "host" ? /[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i:
-        $t eq "url" ? /((https?://|ftp://|www\.|[^\s:=]+@www\.).*?[a-z_\/0-9\-\#=&])(?=(\.|,|;|\?|\!)?("|'|\[|\s|\r|\n|$))/i :
-        0;
-}
-
-print int(get());
+print int(
+    $t eq "email" ? /(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)/i :
+    $t eq "host" ? /[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i :
+    $t eq "url" ? /((https?:\/\/|ftp:\/\/|www\.|[^\s:=]+\@www\.).*?[a-z_\/0-9\-\#=&])(?=(\.|,|;|\?|\!)?("|'|\[|\s|\r|\n|\$))/i :
+    0
+);
