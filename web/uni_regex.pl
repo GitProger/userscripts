b48b4bd8dcd4dd71cb646ebc3c125f244640f4f1
GitProger <51924999+GitProger@users.noreply.github.com>
2020-06-20 20:40:53 +0300

    Update uni_regex.pl

--- web/uni_regex.pl
+++ web/uni_regex.pl
@@ -6,11 +6,16 @@ print "Content-type: text/plain\n\n";
 
 my $cgi = CGI->new();
 my $t = $cgi->param("type") || "";
-$_ = $cgi->param("value") || "";
+$_ = $cgi->param("value") || ""; # '$_' is a default variable for perl regexp's (/.../ as $_ =~ /.../)
 
 print int(
     $t eq "email" ? /(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)/i :
     $t eq "host" ? /[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i :
-    $t eq "url" ? /((https?:\/\/|ftp:\/\/|www\.|[^\s:=]+\@www\.).*?[a-z_\/0-9\-\#=&])(?=(\.|,|;|\?|\!)?("|'|\[|\s|\r|\n|\$))/i :
+    $t eq "url" ? /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:\/?#[\]@!\$&'\(\)\*\+,;=.]+$/i :
     0
 );
+
+# examples returning '1'
+# http://localhost/uni_regex.pl?type=email&value=user@gmail.com
+# http://localhost/uni_regex.pl?type=host&value=www.google.com
+# http://localhost/uni_regex.pl?type=url&value=https://www.google.com/search?q=query/#/js_cmds
