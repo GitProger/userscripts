81ff6b23263b48466c49317b45068a1c344fdb37
Alexander Lelchuk <51924999+GitProger@users.noreply.github.com>
2022-03-14 22:34:02 +0300

    Create proxy-unban.user.js

---
+++ js/proxy-unban.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Youtube Proxy ban bypassing
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       GitProger
+// @match        https://www.google.com/sorry/index*
+// @icon         https://www.google.com/s2/favicons?domain=youtube.com
+// @grant        none
+// ==/UserScript==
+
+// when you are using public proxy in your browser not logged in your account, google ban the ability to watch the video.
+// watching a video is avaible via /embed yt
+
+(function() {
+    'use strict';
+    var yt_html = `<iframe type="text/html" width="640" height="360" frameborder="0" src="https://www.youtube.com/embed/$VIDID?autoplay=1"></iframe>`;
+
+    var g = document.body.innerHTML.match(/youtube\.com\/watch\?v=(\w+)/);
+    var id = "";
+    if (g !== null) {
+        id = g[1];
+    } else {
+        g = document.body.innerHTML.match(/youtu\.be\/(\w+)/);
+        if (g !== null) id = g[1];
+    }
+
+    if (id !== "") {
+        document.body.innerHTML += yt_html.replace("$VIDID", id);
+    }
+})();
