c3b0a97efb8f15ae812cc43e7d483c51fd436fc7
Alexander Lelchuk <51924999+GitProger@users.noreply.github.com>
2023-05-24 12:18:03 +0300

    Update plugin.lisp

--- plugin.lisp
+++ plugin.lisp
@@ -16,27 +16,42 @@ $control distortp (_ "Distort reflected sound?") choice ("Yes" "No")
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defvar sound_speed 343.0)
+(defconstant +roads+ (if (arrayp *track*) (length *track*) 1))
 
-(defun identity (x) x)
-(defun get-abs-time (tm) (/ tm (get-duration 1.0)))
-(defun stereo (mono) (vector (cue mono) (cue mono)))
+(unless (fboundp 'identity)
+  (defun identity (x) x))
+
+(defun range (l r)
+  (if (= l r) nil
+    (cons l (range (1+ l) r))))
+
+(defun get-abs-time (tm)
+  (/ tm (get-duration 1.0)))
+
+(defun stereo (mono)
+  (vector (cue mono) (cue mono)))
+
+(defun poly (c mono)
+  (if (> c 1)
+    (apply #'vector
+           (mapcar (lambda (_) (cue mono)) 
+                   (range 0 c))))
+    (cue mono))
 
 ; old versions: (sum 1 (scale -1 (ramp))); (sine 15 1.0); sin(x)/8 + 1/2
 (defun distort (sig)
   (let ((fade (sum 1 (scale -1 (mult (/ 1.0 4) (sum 1 (sine 5 1.0))))))) ; 1/8; sin: (1 5 15 45 90)
     (sim (mult (cue fade) (cue sig))
-         (stereo (scale 0.02 (noise 1.0))))))
+         (poly +roads+ (scale 0.01 (noise 1.0))))))
 
 (defun reflect (sig room_size coefficient distortp)
   (let* ((delay (* 2 (/ room_size sound_speed)))
          (echo (mult (- 1 coefficient) (cue sig)))
          (blank (const 0 (get-abs-time delay)))
-         (buf (stereo blank)))
+         (buf (poly +roads+ blank)))
     (sim (seq (cue sig) (cue buf))
          (at (get-abs-time delay) 
              (funcall (if (zerop distortp) #'distort #'identity) 
                       (cue echo))))))
-       ; (funcall (if (zerop distortp) #'distort #'identity) 
-       ;     (at (get-abs-time delay) (cue echo)))))))
 
 (reflect *track* room_size coefficient distortp)
